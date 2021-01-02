204e60dd4f765e33904a36f7760abd2bd8f120d5
David Bailey <davidbailey00@outlook.com>
2021-01-02 22:49:03 +0000

    fix timer not working

--- src/components/timer.tsx
+++ src/components/timer.tsx
@@ -5,7 +5,9 @@ function Timer() {
   const [time, setTime] = useState(0);
 
   useEffect(() => {
-    const interval = setInterval(() => setTime(time + 1), 1000);
+    const interval = setInterval(() => {
+      setTime((time) => time + 1);
+    }, 1000);
     return () => clearInterval(interval);
   }, []);
 
