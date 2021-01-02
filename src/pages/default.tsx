5176d3c17229d610fb3298a5a7cfa6653ceeaf2e
David Bailey <davidbailey00@outlook.com>
2021-01-02 19:19:05 +0000

    remove layout to avoid complexity for now

--- src/pages/default.tsx
+++ src/pages/default.tsx
@@ -1,16 +1,15 @@
 import { h } from 'preact';
 
-import DefaultLayout from '../layouts/default';
 import Timer from '../components/timer';
 
 function DefaultPage() {
   return (
-    <DefaultLayout>
+    <main>
       <h1>Hello, world!</h1>
       <p>Rendered with Express</p>
 
       <Timer />
-    </DefaultLayout>
+    </main>
   );
 }
 
