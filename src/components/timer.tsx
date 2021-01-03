8a8475a6b3bf059ade6f2923c0c9c5163152ebca
David Bailey <davidbailey00@outlook.com>
2021-01-03 16:12:43 +0000

    adjust how timer component works

--- src/components/timer.tsx
+++ src/components/timer.tsx
@@ -7,9 +7,11 @@ function Timer() {
   const [time, setTime] = useState(0);
 
   useEffect(() => {
-    const interval = setInterval(() => setTime(time + 1), 1000);
+    const interval = setInterval(() => {
+      setTime((time) => time + 1);
+    }, 1000);
     return () => clearInterval(interval);
-  }, [time, setTime]);
+  }, []);
 
   const b = bem('Timer');
   return (
